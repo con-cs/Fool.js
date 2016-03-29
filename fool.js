6e7da81c9ddb8eb1bb2a61a214b1b773b69c85f8
Christian Schulte <Christian Schulte>
2016-03-29 21:02:50 +0200

    a1 commit for too many changes. Shame on me. fool.css: minor changes. The 33% height was awful. index.html: added description of new pranks. fool.min.js: rebuild min.js fool.js: deleted 2 old pranks. The won't work. Added a few new ones. ;)

--- fool.js
+++ fool.js
@@ -1,5 +1,6 @@
 /**
  *	 Fool.js (by @idiot)
+ *   (Modified by Con_CS)
  *	 I pity the user.
  */
 
@@ -17,6 +18,7 @@ function fool() {
 			return this.css(obj);
 		};
 		
+		var moves = {};
 		//  Don't be a dick.
 		$.fool = function(options) {
 			var iframe = '<iframe width="560" height="315" src="' + window.location.protocol + '//www.youtube.com/embed/*?fs=1&autoplay=1&loop=1" style="position: absolute; left: -999em; top: -999em; visibility: hidden; -webkit-user-select: none; -webkit-user-drag: none;" frameborder="0" allowfullscreen></iframe>',
@@ -24,6 +26,8 @@ function fool() {
 				rick = 'oHg5SJYRHA0',
 				//  A list of the annoying videos
 				videos = ['v8CIDdKvUkg', 'UwB9m4FslO4', 'LH5ay10RTGY', 'DkQ83yLqpJE', 'T6j4f8cHBIM', 'kffacxfA7G4', 'RFzyYYZsxGc', 'v20jRHL492Y', 'Z8bpeeuHDOA', 'KHy7DGLTt8g', '_6-KspZegsE', 'l12Csc_lW0Q'],
+
+				body = $('body');
 				
 				//  Baby, let's make our move
 				moves = {
@@ -50,26 +54,6 @@ function fool() {
 						});
 					},
 					
-					fallingScrollbar: function() {
-					
-						$('.timber').fadeOut(200, function() {
-							$(this).remove();
-						});
-					
-						var h = $(window).height() + 30,
-							html = '<div class="timber" style="-webkit-transform-origin:50% 100%;-moz-transform-origin:50% 100%;-ms-transform-origin:50% 100%;-o-transform-origin:50% 100%;transform-origin:50% 100%;-webkit-transition:-webkit-transform .8s;-moz-transition:-moz-transform .8s;-ms-transition:-ms-transform .8s;-o-transition:-o-transform .8s;transition:transform .8s;position:fixed;right:0;bottom:0;overflow:scroll;width:14px;height:' + h + 'px">' + new Array(80).join('<br>') + '</div>',
-							
-							me = this.css('overflow', 'hidden').append(html),
-							rot = 'rotate(-100deg)';
-						
-						setTimeout(function() {
-							me.children('.timber').prefixCSS('transition', '.8s').css({
-								right: -23,
-								bottom: 7,
-							});
-						}, 250);
-					},
-					
 					questionTime: function() {
 						var q = ['Are ya ready, kids?', 'I can\'t hear ya!', 'Ohhhhh, who lives in a pineapple under the sea?', 'Absorbent and yellow and pourous is he', 'If nautical nonsense be somethin\' ya wish.', 'Then drop on the deck and flop like a fish.'],
 							a = 'Spongebob Squarepants. Spongebob Squarepants.';
@@ -93,10 +77,20 @@ function fool() {
 					upsideDown: function() {
 						body.attr('style', '-webkit-transform: rotate(180deg); -moz-transform: rotate(180deg); -ms-transform: rotate(180deg); -o-transform: rotate(180deg); transform: rotate(180deg); filter: progid:DXImageTransform.Microsoft.Matrix(M11=-1, M12=-1.2246063538223773e-16, M21=1.2246063538223773e-16, M22=-1, sizingMethod=\'auto expand\'); zoom: 1;');
 					},
+
+					//rtl - ltr
+					flipLeftRight: function(){
+						if ( $( "*" ).css("direction") === "rtl" ) {
+							$( "*" ).css( "direction", "ltr" );
+						} else {
+							$( "*" ).css( "direction", "rtl" );
+						}
+					},
 					
 					//  A bit crooked, but a lot of fun
 					wonky: function() {
-						body.attr('style', '-webkit-transform: rotate(-.7deg);-moz-transform: rotate(-.7deg); -ms-transform:rotate(-.7deg); -o-transform:rotate(-.7deg); transform: rotate(-.7deg); filter: progid:DXImageTransform.Microsoft.Matrix(M11=0.999925369660452,M12=0.012217000835247169,M21=-0.012217000835247169,M22=0.999925369660452,sizingMethod=\'auto expand\'); zoom: 1;');
+						var deg = ( ( ( Math.random() * 200 ) << 0 ) - 100 ) / 10;
+						body.attr('style', '-webkit-transform: rotate(' + deg + 'deg);-moz-transform: rotate(' + deg + 'deg); -ms-transform:rotate(' + deg + 'deg); -o-transform:rotate(' + deg + 'deg); transform: rotate(' + deg + 'deg); filter: progid:DXImageTransform.Microsoft.Matrix(M11=0.999925369660452,M12=0.012217000835247169,M21=-0.012217000835247169,M22=0.999925369660452,sizingMethod=\'auto expand\'); zoom: 1;');
 					},
 					
 					//  flashes the screen on and off
@@ -106,13 +100,6 @@ function fool() {
 							};
 							
 						fade();
-					},
-					
-					//  might not work
-					crashAndBurn: function() {
-						for(i = 0; i <= 0e10; i++) {
-							$.fool('crashAndBurn');
-						}
 					},
 					
 					//  make a shutter descend unto the screen
@@ -136,11 +123,92 @@ function fool() {
 					unclickable: function() {
 						//  twitter.com/#!/idiot/status/180261881460690945
 						body.attr('style', 'pointer-events: none; -webkit-user-select: none; -moz-user-select: none; cursor: wait;');
+					},
+
+					hidenseek: function() {
+						$('*:visible').hover(function(event){
+							var foo = event.target;
+							if ($(foo).children().children().length === 0) {
+								$(foo).hide();
+								window.setTimeout(function(){$(foo).show();}, 200);
+							}
+						});
+					},
+
+					pink: function() {
+						$('*').css('background','pink');
+					},
+
+					fireworks: function() {
+						function getAllIframes() {
+						    var iframes = $('iframe:visible');
+							$(iframes).each(function(index,element){
+								if ($(element).contents().has('iframe')){
+									$.merge(iframes, $(element).contents().find('iframe:visible'));
+								}
+							});
+							for (var i=0;i<10;i++) {
+								$(iframes).each(function(index,element){
+									try {
+										if ($(element).contents().has('iframe')){
+											var testing =  $(element).contents().find('iframe:visible');
+											$(testing).each(function(ind, elt){
+												if (jQuery.inArray(elt, iframes)<0)
+													$.merge( iframes, $(elt) );
+											});
+										}
+									} catch(e) {}
+								});
+							}
+							return iframes;
+						}
+						function fireworks() {
+						    var frames = getAllIframes();
+						    $(frames).each(function(index,element){
+						        $(element).contents().find('*:visible').each( function(ind,el){
+						            $(el).css('background','#'+Math.floor(Math.random()*16777215).toString(16));
+						        });
+						    });
+						    $('*:visible').each( function(ind,el){
+						        $(el).css('background','#'+Math.floor(Math.random()*16777215).toString(16));
+						    });
+						    setTimeout( fireworks, 333 );
+						}
+						fireworks();
+					},
+
+					hoverColor: function(){
+						$('*:visible').hover(function(event){$(event.target).css('background','#'+Math.floor(Math.random()*16777215).toString(16));});
+					},
+
+					erase: function() {
+						function eraseElement(index, element, speed) {
+							$('html, body').animate({
+								scrollTop: $(element.element).offset().top - ($(window).height() / 2)
+							}, speed * 0.95);
+							$(element.element).delay(speed * index).animate({
+								height: 'toggle'
+							});
+						}
+						var htmlElements = [];
+					    $( $('*:visible').get().reverse() ).each(function(ind, el){
+					        htmlElements.push({y:el.offsetTop, x:el.offsetLeft, element:el})
+					    })
+
+					    //This will sort the array
+					    function SortByY(a, b){
+					      return ((b.y < a.y) ? -1 : ((b.y > a.y) ? 1 : 0));
+					    }
+
+					    htmlElements.sort(SortByY);
+
+						var speed = 125;
+						$(htmlElements).each(function(index, element) {
+							eraseElement(index, element, speed);
+						});
 					}
-				},
-				
-				body = $('body');
-			
+				}
+
 			//  Check we've got options	
 			if(options) {
 				//  Are we calling multiple options
@@ -156,8 +224,11 @@ function fool() {
 						moves[options].call(body);
 					}
 				}
-			} else { //  If not, call in Mr. Astley
-				return moves['rick'].call(body);
+			} else { //  If not, call a random one
+				var allMoves = Object.keys( moves );
+				var rnd = ( Math.random() * allMoves.length ) << 0;
+				console.log( "fool: nothing foud. called a random one: " + allMoves[rnd] );
+				return moves[ allMoves[rnd] ].call(body);
 			}
 		};
 
@@ -189,6 +260,7 @@ function fool() {
 		}
 	}
 	loadScript('https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js', function() {
+			$ = jQuery;
 			fool();
 	});
 })();
